2831b905aa1e12f38dab5eaa97c23965e39a9ab6
limoore21 <maximburnov08@gmail.com>
2026-06-03 20:06:07 +0400

    Коммит от лизы: Создана таблица дефектов

--- views/defects/index.php
+++ views/defects/index.php
@@ -2,20 +2,34 @@
 
     <h2>Журнал дефектов</h2>
     <table class="table">
-        <thead>
-        <tr><th>ID</th><th>Точка</th><th>Категория</th><th>Критичность</th><th>Статус</th></tr>
-        </thead>
-        <tbody>
-        <?php foreach ($defects as $defect): ?>
-            <tr>
-                <td><?= htmlspecialchars($defect['id']) ?></td>
-                <td><?= htmlspecialchars($defect['point_label']) ?></td>
-                <td><?= htmlspecialchars($defect['category']) ?></td>
-                <td><?= htmlspecialchars($defect['severity']) ?></td>
-                <td><?= htmlspecialchars($defect['status']) ?></td>
-            </tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Точка</th>
+        <th>Категория</th>
+        <th>Критичность</th>
+        <th>Статус</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($defects as $defect): ?>
+        <tr>
+            <td><?= htmlspecialchars($defect['id']) ?></td>
+            <td><?= htmlspecialchars($defect['network_label']) ?></td>
+            <td><?= htmlspecialchars($defect['category']) ?></td>
+            <td>
+                <span class="badge badge-<?= htmlspecialchars($defect['severity']) ?>">
+                    <?= htmlspecialchars($defect['severity']) ?>
+                </span>
+            </td>
+            <td>
+                <span class="badge badge-<?= htmlspecialchars($defect['status']) ?>">
+                    <?= htmlspecialchars($defect['status']) ?>
+                </span>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+    ?>
 
 <?php include '../views/layouts/footer.php'; ?>
