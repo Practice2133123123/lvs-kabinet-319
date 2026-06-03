7d0686b438c48a269ee2737bc39da45338fdddf4
limoore21 <maximburnov08@gmail.com>
2026-06-03 20:15:33 +0400

    Коммит миржана: таблица точек

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'lvs-kabinet-319';
 $user = 'root';
-$pass = '';
+$pass = '1234';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass, [
