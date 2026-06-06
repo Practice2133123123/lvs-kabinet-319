31dd3d36f7701d65ba0ad9a0831eabaf4b22b7ba
Your Name <you@example.com>
2026-06-06 11:01:45 +0000

    refactor: Use __DIR__ for include paths in material add view

--- views/materials/add.php
+++ views/materials/add.php
@@ -1,4 +1,4 @@
-<?php include '../views/layouts/header.php'; ?>
+<?php include __DIR__ . '/../layouts/header.php'; ?>
 
     <h2>Добавить расход материала</h2>
 
@@ -45,4 +45,4 @@
         <a href="materials.php">Отмена</a>
     </form>
 
-<?php include '../views/layouts/footer.php'; ?>
+<?php include __DIR__ . '/../layouts/footer.php'; ?>
