2b2dc99db7753839fdfdf5fe966df03de039789c
Your Name <you@example.com>
2026-06-10 01:27:43 +0400

    docker

--- controllers/defects/defects_edit_controller.php
+++ controllers/defects/defects_edit_controller.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../../models/defects/DefectModel.php';
-require_once __DIR__ . '/../../models/inventory/PointModel.php'; // Для getAllPointsForSelect()
+require_once __DIR__ . '/../../models/inventory/PointModel.php';
 require_once __DIR__ . '/../../models/logs/LogModel.php';
 
 $id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
